4adae8fd71561a116a8efed07daa65db89d57eea
Martin Belev <martinbelev93@gmail.com>
2016-11-27 13:39:48 +0200

    Add tree shaking and build app

--- app/main-aot.ts
+++ app/main-aot.ts
@@ -3,6 +3,9 @@
 import { platformBrowser }    from '@angular/platform-browser';
 
 import { AppModuleNgFactory } from '../aot/app/app.module.ngfactory';
+import {enableProdMode} from "@angular/core";
+
+enableProdMode();
 
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory)
   .then(success => console.log(`Bootstrap success`))
